ac6098f95e7ed9c9f4ce613932b2053ad5cfafc9
JHMejia <29128358+jhmejia@users.noreply.github.com>
2024-03-25 23:23:15 -0500

    add problem1 and 5 c

--- problem1.c
+++ problem1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <omp.h> // Include the OpenMP header
+#include <omp.h> 
 
 int main() {
     #ifdef _OPENMP
         // Check if the _OPENMP macro is defined
         int openmp_version = _OPENMP;
+        
         printf("OpenMP version: %d.%02d\n", openmp_version / 100, openmp_version % 100);
     #else
         printf("OpenMP is not supported.\n");
