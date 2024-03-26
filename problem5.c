6e5444d431e575ad31640d69e50205d1e9795f9f
JHMejia <29128358+jhmejia@users.noreply.github.com>
2024-03-25 23:35:31 -0500

    add problem5.c

--- problem5.c
+++ problem5.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 
-int main() {
+int main(int argc, char* argv[]) {
     int num_iterations;
+    int num_threads;
+
+    int     thread_count;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <number of threads>\n", argv[0]);
+        exit(0);
+    }
+    thread_count = strtol(argv[1], NULL, 10);
+    omp_set_num_threads(thread_count);
 
     printf("Enter the number of iterations: ");
     scanf("%d", &num_iterations);
 
-    #pragma omp parallel
+    #  pragma omp parallel
     {
         int thread_id = omp_get_thread_num();
         int num_threads = omp_get_num_threads();
