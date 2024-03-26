07b811430bbbfdcac95bb05deb1cf7cffa4ecc44
JHMejia <29128358+jhmejia@users.noreply.github.com>
2024-03-25 23:44:23 -0500

    Update problems

--- problem5.c
+++ problem5.c
@@ -1,33 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
+#include <string.h>
+#include <stddef.h>
 
-int main(int argc, char* argv[]) {
-    int num_iterations;
-    int num_threads;
 
-    int     thread_count;
-
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s <number of threads>\n", argv[0]);
-        exit(0);
+/***
+ * strtok implementation that is thread-safe and does not modify the input string.
+ * 
+ * @param input The input string to tokenize. If NULL, the function will continue tokenizing the string from the last context.
+ * @param delim The delimiter string.
+ * @param context The context to store the current position in the input string.
+ * 
+ * @return The next token in the input string or NULL if there are no more tokens.
+*/
+char* strtok_raw_threadsafe(const char* input, const char* delim, char** context)
+{
+    if (!input && !(*context)) {
+        return NULL; // No input string and no context
     }
-    thread_count = strtol(argv[1], NULL, 10);
-    omp_set_num_threads(thread_count);
 
-    printf("Enter the number of iterations: ");
-    scanf("%d", &num_iterations);
+    if (input) {
+        *context = (char*)input; // Initialize context
+    }
 
-    #  pragma omp parallel
-    {
-        int thread_id = omp_get_thread_num();
-        int num_threads = omp_get_num_threads();
+    char* start = *context;
+    if (*start == '\0') {
+        return NULL; // We've reached the end of the string
+    }
 
-        #pragma omp for
-        for (int i = 0; i < num_iterations; ++i) {
-            printf("Thread %d: Iteration %d\n", thread_id, i);
+    // Look for the end of the token
+    char* end = start;
+    while (*end) {
+        if (strchr(delim, *end)) {
+            break;
         }
+        end++;
     }
 
+    // Allocate memory for the token
+    ptrdiff_t tokenLength = end - start;
+    char* token = (char*)malloc(tokenLength + 1);
+
+    if (!token) {
+        return NULL; // Allocation failure
+    }
+
+    strncpy(token, start, tokenLength);
+    token[tokenLength] = '\0';
+
+    *context = *end ? end + 1 : end; // Update the context
+
+    return token;
+}
+
+void free_token(char* token) {
+    free(token);
+}
+
+int main() {
+    const char* inputString = "The quick brown fox jumps over the lazy dog";
+    const char* delimiters = " ";
+
+    char* context = NULL;
+    char* token = strtok_raw_threadsafe(inputString, delimiters, &context);
+    while (token != NULL) {
+        printf("Token: %s\n", token);
+        free_token(token); // Free the memory allocated for token
+        token = strtok_raw_threadsafe(NULL, delimiters, &context);
+    }
     return 0;
 }
